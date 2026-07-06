90a13c1c2c9152a3de44045a7c1b3690501b4e61
madhuradarshinirm <madhuradarshinirm@gmail.com>
2026-07-06 21:29:32 +0530

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,5 +1,4 @@
 from utils.firebase_config import get_firestore
-import streamlit as st
 
 db = get_firestore()
 
@@ -8,15 +7,11 @@ def login(email, password):
     doc = db.collection("users").document(email).get()
 
     if not doc.exists:
-        st.error(f"Document not found: {email}")
         return None
 
     user = doc.to_dict()
 
-    st.write("Document data:")
-    st.json(user)
+    if user["password"] != password:
+        return None
 
-    st.write("Keys:")
-    st.write(list(user.keys()))
-
-    return None
+    return user
